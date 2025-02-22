f3fe5cae60658ae22bf04fa806f862ac99da6b85
trippymajo <elyadesu@yandex.ru>
2025-02-22 17:46:01 +0200

    Refactoring.

--- Program.cs
+++ Program.cs
@@ -3,39 +3,105 @@ using System.Resources;
 using System.Collections;
 using System.IO;
 
-
 namespace ResourceReader
 {
-    class ReadResXResources
+    class ResXProc
     {
-        public static void Main()
-        {
-            var dir = new DirectoryInfo(@"C\:example\path"); // Path where RESX files are in
-            StreamWriter sw = new StreamWriter(@"C:\strings.txt"); // Path for saving txt file
+        private readonly string _dirPath;
+        private readonly string _outputFullFileName;
 
-            foreach (FileInfo file in dir.GetFiles())
+        /// <summary>
+        /// Constructor with parameters
+        /// </summary>
+        /// <param name="dirPath">Directory path to process .resx files</param>
+        /// <param name="outputFullFileName">Full file name of the output file</param>
+        public ResXProc(string dirPath, string outputFullFileName)
+        {
+            _dirPath = dirPath;
+            _outputFullFileName = outputFullFileName;
+        }
+
+        /// <summary>
+        /// Starts the Process of the parsing
+        /// </summary>
+        public void Process()
+        {
+            if (!Directory.Exists(_dirPath))
             {
-                string filename = Path.GetFileName(file.FullName);
-                Console.WriteLine("###" + filename + "###\n");
-                sw.WriteLine("###" + filename + "###\n");
-                sw.Flush();
-                ResXResourceReader rsxr = new ResXResourceReader(dir + "\\" + filename); // Create a ResXResourceReader for the file
-                foreach (DictionaryEntry d in rsxr) // Iterate through the resources and display the contents to the console.
-                {
-                    Console.WriteLine("\"" + d.Value.ToString() + "\"");
-                    Console.WriteLine("\"" + d.Value.ToString() + "\"\n");
-                    sw.WriteLine("\"" + d.Value.ToString() + "\"");
-                    sw.Flush();
-                    sw.WriteLine("\"" + d.Value.ToString() + "\"\n");
-                    sw.Flush();
-                }
-                Console.WriteLine("###" + filename + "###\n");
-                sw.WriteLine("###" + filename + "###\n");
-                sw.Flush();
+                Console.WriteLine($"Error: Directory '{_dirPath}' does not exist.");
+                return;
             }
 
-            Console.ReadKey(true); //Close the reader.
+            DirectoryInfo dirInfo = new DirectoryInfo(_dirPath);
+            FileInfo[] resxFiles = dirInfo.GetFiles("*.resx");
+
+            using (var sw = new StreamWriter(_outputFullFileName))
+            {
+                foreach (FileInfo file in resxFiles)
+                {
+                    ProcessSingleFile(file, sw);
+                }
+            }
+
+            Console.WriteLine("Processing complete. Press any key to exit.");
+            Console.ReadKey(true);
+        }
+
+        /// <summary>
+        /// Processes single file from the directory
+        /// </summary>
+        /// <param name="file">File to process</param>
+        /// <param name="sw">Stream writer for the output file</param>
+        private void ProcessSingleFile(FileInfo file, StreamWriter sw)
+        {
+            string filename = file.Name;
+            string filePath = file.FullName;
+
+            Console.WriteLine($"### {filename} ###\n");
+            sw.WriteLine($"### {filename} ###\n");
+
+            try
+            {
+                ReadResxContents(filePath, sw);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error reading file {filename}: {ex.Message}");
+                sw.WriteLine($"Error reading file {filename}: {ex.Message}");
+            }
+
+            Console.WriteLine($"### {filename} ###\n");
+            sw.WriteLine($"### {filename} ###\n");
+        }
+
+        /// <summary>
+        /// ResX contents reading
+        /// </summary>
+        /// <param name="filePath">Path to resX file</param>
+        /// <param name="sw">Stream writer for the output file</param>
+        private void ReadResxContents(string filePath, StreamWriter sw)
+        {
+            using (var rsxr = new ResXResourceReader(filePath))
+            {
+                foreach (DictionaryEntry entry in rsxr)
+                {
+                    string value = entry.Value != null ? entry.Value.ToString() : "(null)";
+                    Console.WriteLine($"\"{value}\"");
+                    sw.WriteLine($"\"{value}\"");
+                }
+            }
         }
     }
 
+    class Program
+    {
+        static void Main()
+        {
+            string directoryPath = @"C:\example\path";
+            string outputFilePath = @"C:\strings.txt";
+
+            ResXProc processor = new ResXProc(directoryPath, outputFilePath);
+            processor.Process();
+        }
+    }
 }
--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("GetStringsFromRESX")]
+[assembly: AssemblyTitle("ResXdotNetReader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GetStringsFromRESX")]
-[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyProduct("ResXdotNetReader")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("eeb774c6-e276-4724-a338-7be16d35f579")]
+[assembly: Guid("15b6fd58-ee43-4c9b-a2cb-c706da680f3e")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -29,8 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
